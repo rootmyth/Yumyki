c8e5a59416e990fb46372bac3d83a2d73645faa3
rootmyth <rootmyth@gmail.com>
2022-12-11 20:42:57 -0600

    Created Recipe Repository Interface.

---
+++ Yumyki/Interfaces/IRecipeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Yumyki.Models;
+
+namespace Yumyki.Interfaces
+{
+    public interface IRecipeRepository
+    {
+        List<Recipe> GetAllRecipes();
+        List<RecipeIngredient> GetRecipeIngredients(int recipeId);
+        List<InstructionStep> GetRecipeInstructions(int recipeId);
+        void InsertRecipeTableValues(Recipe recipe);
+        void InsertIngredientTableValues(Recipe recipe);
+        void InsertRecipeIngredientTableValues(Recipe recipe);
+        void InsertInstructionStepTableValues(Recipe recipe);
+        void DeleteRecipe(int recipeId);
+    }
+}
