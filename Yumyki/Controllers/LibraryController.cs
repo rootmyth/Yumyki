756d91fa66c8726fe2f491402f504ada490a2a18
rootmyth <rootmyth@gmail.com>
2022-12-13 19:41:15 -0600

    Created Library Controller.

---
+++ Yumyki/Controllers/LibraryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Yumyki.Interfaces;
+using Yumyki.Models;
+
+namespace Yumyki.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibraryController : ControllerBase
+    {
+        private readonly ILibraryRepository _libraryRepo;
+        public LibraryController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepo = libraryRepository;
+        }
+
+        public List<Recipe> GetLibraryRecipes(int userId)
+        {
+            return _libraryRepo.GetLibraryRecipes(userId);
+        }
+        public void PostRecipeToLibrary(int userId, int recipeId)
+        {
+            _libraryRepo.PostRecipeToLibrary(userId, recipeId);
+        }
+        public void DeleteRecipeFromLibrary(int Id)
+        {
+            _libraryRepo.DeleteRecipeFromLibrary(Id);
+        }
+    }
+}
