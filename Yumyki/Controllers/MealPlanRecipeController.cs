cf90ad75fd17a21c0571291edd483645badbd5bb
rootmyth <rootmyth@gmail.com>
2022-12-13 19:43:01 -0600

    Created MealPlanRecipe Controller.

---
+++ Yumyki/Controllers/MealPlanRecipeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Yumyki.Interfaces;
+using Yumyki.Models;
+
+namespace Yumyki.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MealPlanRecipeController : ControllerBase
+    {
+        private readonly IMealPlanRecipeRepository _mealPlanRecipeRepo;
+        public MealPlanRecipeController(IMealPlanRecipeRepository mealPlanRecipeRepository)
+        {
+            _mealPlanRecipeRepo = mealPlanRecipeRepository;
+        }
+        //Get
+        [HttpGet]
+        public List<MealPlanRecipe> GetMealPlanRecipes(int mealPlanId)
+        {
+            return _mealPlanRecipeRepo.GetMealPlanRecipes(mealPlanId);
+        }
+
+        //Post
+        public void PostMealPlanRecipe(int recipeId, int mealPlanId)
+        {
+            _mealPlanRecipeRepo.PostRecipeToMealPlan(recipeId, mealPlanId);
+        }
+
+        //Delete
+        public void DeleteMealPlanRecipe(int id)
+        {
+            _mealPlanRecipeRepo.DeleteMealPlanRecipe(id);
+        }
+
+
+        //Complete
+        public void CompleteMealPlanRecipe(int id)
+        {
+            _mealPlanRecipeRepo.CompleteMealPlanRecipe(id);
+        }
+    }
+}
