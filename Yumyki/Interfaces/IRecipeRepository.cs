4e065c3ce3cb22ed694598f4a7572f890cca4af2
rootmyth <rootmyth@gmail.com>
2022-12-13 20:45:02 -0600

    Removed GetRecipeIngredients and GetRecipeInstructions methods to put in their own Interfaces.

--- Yumyki/Interfaces/IRecipeRepository.cs
+++ Yumyki/Interfaces/IRecipeRepository.cs
@@ -5,12 +5,11 @@ namespace Yumyki.Interfaces
     public interface IRecipeRepository
     {
         List<Recipe> GetAllRecipes();
-        List<RecipeIngredient> GetRecipeIngredients(int recipeId);
-        List<InstructionStep> GetRecipeInstructions(int recipeId);
         void InsertRecipeTableValues(Recipe recipe);
         void InsertIngredientTableValues(Recipe recipe);
         void InsertRecipeIngredientTableValues(Recipe recipe);
         void InsertInstructionStepTableValues(Recipe recipe);
         void DeleteRecipe(int recipeId);
+        void UpdateRecipe(Recipe recipe);
     }
 }
