e4028b378a38779ef0a136b364f15b26d03acf83
rootmyth <rootmyth@gmail.com>
2022-12-13 20:48:15 -0600

    Removed nullable Ingredient, now non-nullable.

--- Yumyki/Models/RecipeIngredient.cs
+++ Yumyki/Models/RecipeIngredient.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int RecipeId { get; set; }
         public int IngredientId { get; set; }
-        public Ingredient? Ingredient { get; set; }
+        public Ingredient Ingredient { get; set; }
         public decimal Quantity { get; set; }
         public string? QuantityUnit { get; set; }
         public string? Note { get; set; }
