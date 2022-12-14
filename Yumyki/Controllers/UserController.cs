1938f041c43a818883fe05f885ed3dce82cd3351
rootmyth <rootmyth@gmail.com>
2022-12-13 20:42:41 -0600

    Created User Controller.

---
+++ Yumyki/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Yumyki.Interfaces;
+using Yumyki.Models;
+
+namespace Yumyki.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepo;
+        public UserController(IUserRepository usereRepository)
+        {
+            _userRepo = usereRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetUser(string firebaseId)
+        {
+            User user = _userRepo.GetUser(firebaseId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpGet("DoesUserExist")]
+        public IActionResult CheckIfUserExists(string firebaseId)
+        {
+            User user = _userRepo.GetUser(firebaseId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        [HttpPost]
+        public void PostUser(User user)
+        {
+            _userRepo.PostUser(user);
+        }
+    }
+}
