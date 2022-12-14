945e9c9ead620470f80032de39a05b8e9546e4e4
rootmyth <rootmyth@gmail.com>
2022-12-13 19:40:56 -0600

    Created InstructionStep Controller.

---
+++ Yumyki/Controllers/InstructionStepController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Yumyki.Interfaces;
+using Yumyki.Models;
+
+namespace Yumyki.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstructionStepController : ControllerBase
+    {
+        private readonly IInstructionStepRepository _InstructionStepRepo;
+        public InstructionStepController(IInstructionStepRepository instructionStepRepository)
+        {
+            _InstructionStepRepo = instructionStepRepository;
+        }
+
+        [HttpGet("Instructions/{recipeId}")]
+        public List<InstructionStep> GetRecipeInstructions(int recipeId)
+        {
+            return _InstructionStepRepo.GetRecipeInstructions(recipeId);
+        }
+    }
+}
