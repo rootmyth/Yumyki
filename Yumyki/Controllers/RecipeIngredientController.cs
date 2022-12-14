39c7cc6d2c22887e68178292cee819c082b8c409
rootmyth <rootmyth@gmail.com>
2022-12-13 20:41:59 -0600

    Created RecipeIngredient Controller.

---
+++ Yumyki/Controllers/RecipeIngredientController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Yumyki.Interfaces;
+using Yumyki.Models;
+
+namespace Yumyki.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeIngredientController : ControllerBase
+    {
+        private readonly IRecipeIngredientRepository _recipeIngredientRepo;
+        public RecipeIngredientController(IRecipeIngredientRepository recipeRepository)
+        {
+            _recipeIngredientRepo = recipeRepository;
+        }
+
+        [HttpGet]
+        public List<RecipeIngredient> GetRecipeIngredients(int recipeId)
+        {
+            return _recipeIngredientRepo.GetRecipeIngredients(recipeId);
+        }
+
+        [HttpPut]
+        public void UpdateRecipeIngredients(List<RecipeIngredient> recipeIngredientList)
+        {
+            _recipeIngredientRepo.InsertIngredientTableValues(recipeIngredientList);
+            _recipeIngredientRepo.UpdateRecipeIngredients(recipeIngredientList);
+        }
+    }
+}
